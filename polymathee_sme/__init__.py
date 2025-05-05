35cf44a91eb555f2847098c056f44436fa43722f
Nayren <rchouchane@iut.univ-paris8.fr>
2025-05-05 22:58:50 +0200

    correction CORS

--- polymathee_sme/__init__.py
+++ polymathee_sme/__init__.py
@@ -1,4 +1,5 @@
 """Initialisation of the api"""
+
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -9,7 +10,9 @@ from polymathee_sme.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-cors = CORS(app, resources={r"*": {"origins": Config.FRONT_END_URL}}, supports_credentials=True)
+print(Config.FRONT_END_URL)
+# cors = CORS(app, resources={r"*": {"origins": Config.FRONT_END_URL}}, supports_credentials=True)
+CORS(app, resources={r"*": {"origins": "http://localhost:4200"}}, supports_credentials=True)
 api = Api(app)
 jwt = JWTManager(app)
 rq = RQ(app)
